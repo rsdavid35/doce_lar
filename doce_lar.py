6e1a042bfbc83f2ba65c94701670d9ea0a48258f
David Luiz Silva Rodrigues <rsdavid35@gmail.com>
2023-01-23 01:16:45 -0300

    Arquivos do projeto
    Adição dos arquivos.

---
+++ doce_lar.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 22 13:04:29 2023
+
+@author: David Rodrigues
+"""
+
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('Housing.csv')[['price', 'area', 'bedrooms', 'bathrooms']]
+
+# retorna o conjunto de dados
+def obter_dados():
+    return data
+
+def dados_input():
+    range_area = (data['area'].min(), data['area'].max())
+    arr_quarto = np.sort(data['bedrooms'].value_counts().index)
+    arr_banheiro = np.sort(data['bathrooms'].value_counts().index)
+    return {'range_area': range_area,
+            'arr_quarto': arr_quarto,
+            'arr_banheiro': arr_quarto}
+
+# filtra os dados de acordo com as informações passadas
+def filtrar_dados(area, bedrooms, bathrooms, df=data):
+    df = df.loc[(area[0]<=df['area'])&(df['area']<area[1])]
+    df = df.loc[df['bedrooms']==bedrooms]
+    df = df.loc[df['bathrooms']==bathrooms]
+    return df
+
+# calcula os indicadores da interface
+def stats_dados(df=data):
+    media_valor = round(df['price'].mean(), 2)
+    media_geral = round(data['price'].mean(), 2)
+    delta = round(media_valor - media_geral, 2)
+    menor_valor = df['price'].min()
+    maior_valor = df['price'].max()
+    arr_valor = df['price']
+    return {'media_valor': media_valor,
+            'media_geral':media_geral,
+            'delta':delta,
+            'menor_valor':menor_valor,
+            'maior_valor':maior_valor,
+            'arr_valor': arr_valor}
---
+++ interface.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 22 20:53:31 2023
+
+@author: David Rodrigues
+"""
+
+from doce_lar import *
+from PIL import Image
+import streamlit as st
+import locale
+locale.setlocale( locale.LC_ALL, 'US' )
+
+#title
+st.title('Doce Lar')
+
+# imagem
+image = Image.open('img/hs.jpg')
+st.image(image)
+
+# inputs
+range_area = dados_input()['range_area']
+arr_quarto = dados_input()['arr_quarto']
+arr_banheiro = dados_input()['arr_banheiro']
+
+col_area, col_quarto, col_banheiro = st.columns(3)
+with col_area:
+    input_area = st.slider('Área [SqFt]',
+                           value=range_area,
+                           min_value=range_area[0],
+                           max_value=range_area[1],)
+with col_quarto:
+    input_quarto = st.selectbox('Número de quartos', arr_quarto)
+with col_banheiro:
+    input_banheiro =st.selectbox('Número de banheiros', arr_quarto)
+    
+# indicadores
+dados_filtrados = filtrar_dados(input_area,
+                                int(input_quarto),
+                                int(input_banheiro))
+ind = stats_dados(dados_filtrados)
+
+col_media, col_max, col_min = st.columns(3)
+with col_media:
+    if ind['delta'] >= 0:
+        delta = "${:,.2f}".format(ind['delta']/10**6)
+    else:
+        delta = "-${:,.2f}".format(abs(ind['delta']/10**6))
+    st.metric('Média de Preço (em milhões)',
+              value=locale.currency( ind['media_valor']/10**6, grouping=True ),
+              delta= delta)
+    st.caption('Comparação com a Média Geral')
+with col_min:
+    st.metric('Menor Preço (em milhões)',
+              value=locale.currency( ind['menor_valor']/10**6, grouping=True ))
+    with col_max:
+        st.metric('Maior Preço (em milhões)',
+                  value=locale.currency( ind['maior_valor']/10**6, grouping=True ))
+      
+# tabela
+col_tabela, col_descricao = st.columns(2)
+with col_tabela:
+    st.dataframe(dados_filtrados, height=200)
+
+with col_descricao:
+    st.write('Você pode baixar os dados filtrados clicando no botão abaixo.')
+    
+    # baixar dados
+    @st.cache
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+    
+    csv = convert_df(dados_filtrados)
+    
+    st.download_button(
+        label="Baixar dados",
+        data=csv,
+        file_name='housing.csv',
+        mime='text/csv',
+    )
